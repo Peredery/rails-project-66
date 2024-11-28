174ca227cad08c85c03b15fe5a5474581a9a3b7e
A.V. Peredery <peredery@pm.me>
2024-11-28 23:00:39 +0500

    fix(test): revert sign_in from diff

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -4,41 +4,33 @@ module Authentication
   extend ActiveSupport::Concern
 
   included do
-    before_action :authenticate_user_from_session
-    protect_from_forgery with: :exception
-
-    helper_method :current_user, :signed_in?
-
-    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-  end
-
-  def user_not_authorized
-    redirect_to request.referer || root_path, alert: t('not_authorized')
+    helper_method :current_user, :user_signed_in?
   end
 
   def authenticate_user!
-    redirect_to root_path, alert: t('need_login') unless authenticate_user_from_session
-  end
-
-  def authenticate_user_from_session
-    Current.user ||= User.find_by(id: session[:user_id])
+    redirect_to root_path, alert: t('need_login') unless current_user
   end
 
   def current_user
     Current.user || authenticate_user_from_session
   end
 
-  def signed_in?
+  def user_signed_in?
     current_user.present?
   end
 
+  def authenticate_user_from_session
+    User.find_by(id: session[:user_id])
+  end
+
   def sign_in(user)
+    Current.user = user
     reset_session
     session[:user_id] = user.id
   end
 
   def sign_out
+    Current.user = nil
     reset_session
-    redirect_to root_path, notice: t('logout.success')
   end
 end
--- app/controllers/web/auth_controller.rb
+++ app/controllers/web/auth_controller.rb
@@ -17,5 +17,7 @@ class Web::AuthController < Web::ApplicationController
 
   def destroy
     sign_out
+
+    redirect_to root_path, notice: t('.logout.success')
   end
 end
