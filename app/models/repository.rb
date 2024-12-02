566a2c35a48276c6933f9a33579ba1a0c24b8087
A.V. Peredery <peredery@pm.me>
2024-12-02 19:25:56 +0500

    refactore(model): remove inverse_of

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -27,7 +27,7 @@ class Repository < ApplicationRecord
   extend Enumerize
 
   belongs_to :user
-  has_many :checks, class_name: 'Repository::Check', dependent: :destroy, inverse_of: :repository
+  has_many :checks, class_name: 'Repository::Check', dependent: :destroy
 
   enumerize :language, in: %i[javascript ruby], default: nil, allow_nil: true
 end
