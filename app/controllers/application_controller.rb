0571fbb46e8471a7393c8b357d9bfcbade9df94d
A.V. Peredery <peredery@pm.me>
2024-12-02 20:05:30 +0500

    refactore(auth): rescue pandit and move includes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  include Pundit::Authorization
-  include Authentication
 end
--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class Web::ApplicationController < ApplicationController
+  include Pundit::Authorization
+  include Authentication
 end
