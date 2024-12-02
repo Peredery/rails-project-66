4d95c43bbfe4589c3b5cccaa57fe9b2a0c8ae1f7
A.V. Peredery <peredery@pm.me>
2024-12-02 19:33:39 +0500

    refactore(views): remove blob

--- app/helpers/web/repositories/checks_helper.rb
+++ app/helpers/web/repositories/checks_helper.rb
@@ -7,7 +7,7 @@ module Web::Repositories::ChecksHelper
     "#{GITHUB_URL}/#{repository.full_name}/commit/#{sha}"
   end
 
-  def github_file_link(repository, path, sha)
-    "#{GITHUB_URL}/#{repository.full_name}/blob/#{sha}/#{path}"
+  def github_file_link(repository, path)
+    "#{GITHUB_URL}/#{repository.full_name}/blob/#{path}"
   end
 end
