e67b42582bb31cdc2576bb20d1ed3995656a15d8
A.V. Peredery <peredery@pm.me>
2024-12-02 21:06:38 +0500

    fix(test): checks controller

--- app/controllers/web/repositories/checks_controller.rb
+++ app/controllers/web/repositories/checks_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Web::Repositories::ChecksController < ApplicationController
+class Web::Repositories::ChecksController < Web::ApplicationController
   before_action :authenticate_user!
 
   def show
