5f292a620efe188a8b843f52fcdfff2dd7e6a9e0
A.V. Peredery <peredery@pm.me>
2024-12-02 21:12:23 +0500

    fix(test): save before check count

--- test/controllers/api/checks_controller_test.rb
+++ test/controllers/api/checks_controller_test.rb
@@ -4,14 +4,15 @@ require 'test_helper'
 
 class Api::ChecksControllerTest < ActionDispatch::IntegrationTest
   test 'should create new' do
-    repository = repositories(:one)
+    repository = repositories(:two)
     body = "repository[id]=#{repository.github_id}"
     x_hmac = calculate_x_hmac(body)
     params = { repository: { id: repository.github_id } }
-    repository.checks.count
+    before_check_count = repository.checks.count
 
     post(api_checks_url, headers: { 'HTTP_X_HUB_SIGNATURE_256' => x_hmac }, params:)
     assert_response :success
+    assert { repository.checks.count == before_check_count + 1 }
     assert { repository.checks.last.finished? }
   end
 end
